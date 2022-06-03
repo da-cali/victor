569c7b74419ea1004a773d340e026ca745cbd425
Dan Castillo <10303539-dacali@users.noreply.gitlab.com>
2022-06-03 16:23:03 -0700

    feat: safer spymaster

--- spymaster.py
+++ spymaster.py
@@ -1,40 +1,42 @@
-from joblib import load, dump
-from gensim import downloader
 from fuzzywuzzy import fuzz
 
-# Load local version of model. Download and store otherwise.
-print("Loading model...")
-try:
-    model = load("model.pkl")
-except Exception:
-    model = downloader.load('word2vec-google-news-300')
-    dump(model, "model.pkl")
-
-# Returns a list of tuples (clue_objectives,potential_clues) given a set of words.
+# Returns a list of tuples (words_to_guess,clues) given a set of words.
 # This function uses word embeddings to generate objectives (iterating over every
 # word to find similar words within the set) and clues for the objectives.
-# To do: Refactor to consider list of negative, neutral, and assasin words.
-def spymaster_clues(set_of_words):
-    objectives_and_clues = []
-    for word in set_of_words:
-        similar_words = model.most_similar(word,topn=100000)
-        similar_words_within_set = [w for w,_ in similar_words if w in set_of_words]
-        clue_objectives = sorted([word]+similar_words_within_set)
-        if len(clue_objectives) > 1 and clue_objectives not in [o for o,_ in objectives_and_clues]:
-            potential_clues = model.most_similar(positive=clue_objectives,topn=20)
-            filtered_potential_clues = [(clue,score) for clue,score in potential_clues 
-                                        if "_" not in clue and all([fuzz.partial_ratio(w,clue)<65 for w in set_of_words])]
-            objectives_and_clues.append((clue_objectives,filtered_potential_clues))
-    return sorted(objectives_and_clues,key=(lambda tup: tup[1][0][1]),reverse=True)
-
-# Run the spymaster function on a custom input set of words.
-while True:
-    spymaster_words = input("\nEnter a set of words separated by spaces, or press enter to use example set of words.\n")
-    if spymaster_words == "":
-        with open("words/red.txt","r") as file:
-            set_of_words = [word for word in file.read().split("\n") if word != ""]
-        print(f"Example clues from the set {set_of_words}:")
-    else:
-        set_of_words = spymaster_words.split()
-    print("\nPotential clues:")
-    [print(f"\nObjective: \n{objective} \nClues:\n {clues}") for objective,clues in spymaster_clues(set_of_words)]
+def spymaster_clue(model,ally_agents,opposite_agents,bystanders,assasin):
+    # Generate candidates for clues by finding groups of similar words
+    # within the ally_agents and looking for related words to all of them.
+    candidates = []
+    MAX_WORDS_TO_CONSIDER = 100000
+    THRESH = 65
+    for word in ally_agents:
+        similar_words = model.most_similar(word,topn=MAX_WORDS_TO_CONSIDER)
+        similar_words_within_set = [w for w,_ in similar_words if w in ally_agents]
+        words_to_guess = sorted([word]+similar_words_within_set)
+        if words_to_guess not in [wtg for wtg,_ in candidates]:
+            raw_potential_clues = model.most_similar(positive=words_to_guess,topn=50)
+            legal_potential_clues = [(clue,score) for clue,score in raw_potential_clues 
+                                    if "_" not in clue and all([fuzz.partial_ratio(w,clue)<THRESH for w in ally_agents])]
+            refined_potential_clues = []
+            for clue,score in legal_potential_clues:
+                if all([fuzz.partial_ratio(clue,refined_clue)<THRESH for refined_clue in refined_potential_clues]):
+                    refined_potential_clues.append((clue,score))
+            candidates.append((words_to_guess,refined_potential_clues))
+    # Group the candidates into groups of the same length, sorted by length.
+    groups = {}
+    for wtg,clues in candidates:
+        length = len(wtg)
+        if length in groups:
+            groups[length].append((wtg,clues))
+        else:
+            groups[length] = [(wtg,clues)]
+    sorted_groups = [groups[n] for n in sorted(groups,reverse=True)]
+    # Return best clue filtering out clues for non ally agents.
+    for group in sorted_groups:
+        sorted_candidates = sorted(group,key=(lambda tup: tup[1][0][1]),reverse=True)
+        for objective,clues in sorted_candidates:
+            for clue,_ in clues:
+                if all([word not in model.most_similar(clue,topn=MAX_WORDS_TO_CONSIDER) for word in (opposite_agents+bystanders+assasin)]):
+                    return (objective,clue)
+    first_candidate = sorted_groups[0][0]
+    return (first_candidate[0],first_candidate[0][0])
