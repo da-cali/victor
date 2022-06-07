86609750bfecc557a4b02a2b90099f0f16ac1080
Dan Castillo <46359427+da-cali@users.noreply.github.com>
2022-06-07 12:48:08 -0700

    style: code cleanup

--- play.py
+++ play.py
@@ -11,43 +11,46 @@ except Exception:
     model = downloader.load('word2vec-google-news-300')
     dump(model, "model.pkl")
 
-def play(t1,t1_name,t2,t2_name,b,a):
+# Simulates a Codenames game and returns the result as a string.
+def play(t1_agents,t1_name,t2_agents,t2_name,bystanders,assasin):
     result = "Next turn."
-    print(f"\n--------- {t1_name} team turn ---------\n\n{t1_name} spymaster:")
-    objective,clue = spymaster_clue(model,t1,t2,b,a)
-    print(f"Objective: {objective} \nClue: '{clue}' for {len(objective)}.")
+    print(f"\n--------- {t1_name} team turn ---------\n")
+    print(f"{t1_name} spymaster:")
+    objective,clue = spymaster_clue(model,t1_agents,t2_agents,bystanders,assasin)
+    objective_size = len(objective)
+    print(f"Objective: {objective} \nClue: '{clue}' for {objective_size}.")
     print(f"\n{t1_name} operatives:")
-    guesses = operative_guesses(model,(t1+t2+b+a),clue)
-    for i in range(len(objective)):
+    guesses = operative_guesses(model,(t1_agents+t2_agents+bystanders+assasin),clue)
+    for i in range(objective_size):
         guess = guesses[i][1]
         print(f"Guess: {guess}")
-        if guess in a:
-            result = f"{t1_name} team loses."
+        if guess in assasin:
+            result = f"☠️ Assasin. {t1_name} team loses."
             break
-        elif guess in b:
-            b.remove(guess)
+        elif guess in bystanders:
+            bystanders.remove(guess)
             print("✖️")
             break
-        elif guess in t2:
+        elif guess in t2_agents:
             print("✖️")
-            t2.remove(guess)
-            if t2 == []:
+            t2_agents.remove(guess)
+            if t2_agents == []:
                 result = f"{t2_name} team wins."
             break
         else:
             print("✔️")
-            t1.remove(guess)
-            if t1 == []:
+            t1_agents.remove(guess)
+            if t1_agents == []:
                 result = f"{t1_name} team wins."
                 break
-            if len(objective)-1 == i:
+            if objective_size - 1 == i:
                 break
     if result == "Next turn.":
-        return play(t2,t2_name,t1,t1_name,b,a)
+        return play(t2_agents,t2_name,t1_agents,t1_name,bystanders,assasin)
     else:
         return result
 
-# Play.
+# Set up game and play.
 print("\nPlease enter the codenames (separated by spaces).")
 red = input("\nRed agents:\n").split()
 blue = input("\nBlue agents:\n").split()
