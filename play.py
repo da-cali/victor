795dd7efe7ffd43049f6e28ff9cf9852d9d371db
Dan Castillo <46359427+da-cali@users.noreply.github.com>
2022-11-11 01:35:12 -0800

    style improvements

--- play.py
+++ play.py
@@ -3,6 +3,7 @@ from gensim import downloader
 from spymaster import spymaster_clue
 from operative import operative_guesses
 
+
 # Load local version of model. Download and store otherwise.
 print("\nLoading model...")
 try:
@@ -11,16 +12,17 @@ except Exception:
     model = downloader.load('word2vec-google-news-300')
     dump(model, "model.pkl")
 
+
 # Simulates a Codenames game and returns the result as a string.
-def play(t1_agents,t1_name,t2_agents,t2_name,bystanders,assasin):
+def play(t1_agents, t1_name, t2_agents, t2_name, bystanders, assasin):
     result = "Next turn."
-    print(f"\n--------- {t1_name} team turn ---------\n")
-    print(f"{t1_name} spymaster:")
-    objective,clue = spymaster_clue(model,t1_agents,t2_agents,bystanders,assasin)
+    print(f"\n<= {t1_name} team turn =>\n{t1_name} spymaster:")
+    objective, clue = spymaster_clue(model, t1_agents, t2_agents, bystanders, assasin)
     objective_size = len(objective)
-    print(f"Objective: {objective} \nClue: '{clue}' for {objective_size}.")
-    print(f"\n{t1_name} operatives:")
-    guesses = operative_guesses(model,(t1_agents+t2_agents+bystanders+assasin),clue)
+    print(f"Objective: {objective}\n"
+          + f"Clue: '{clue}' for {objective_size}."
+          + f"\n{t1_name} operatives:")
+    guesses = operative_guesses(model, (t1_agents + t2_agents + bystanders + assasin), clue)
     for i in range(objective_size):
         guess = guesses[i][1]
         print(f"Guess: {guess}")
@@ -46,9 +48,10 @@ def play(t1_agents,t1_name,t2_agents,t2_name,bystanders,assasin):
             if objective_size - 1 == i:
                 break
     if result == "Next turn.":
-        return play(t2_agents,t2_name,t1_agents,t1_name,bystanders,assasin)
+        return play(t2_agents, t2_name, t1_agents, t1_name, bystanders, assasin)
     else:
         return result
+
 
 # Set up game and play.
 print("\nPlease enter the codenames (separated by spaces).")
@@ -56,5 +59,5 @@ red = input("\nRed agents:\n").split()
 blue = input("\nBlue agents:\n").split()
 yellow = input("\nBystanders:\n").split()
 black = input("\nAssasin:\n").split()
-result = play(red,"RED",blue,"BLUE",yellow,black)
+result = play(red, "RED", blue, "BLUE", yellow, black)
 print(f"\nGame over. {result}")
