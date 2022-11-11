fc372ae4964db0713625865ea33d5e915ae92f21
Dan Castillo <46359427+da-cali@users.noreply.github.com>
2022-11-11 01:38:00 -0800

    style improvements

--- operative.py
+++ operative.py
@@ -1,4 +1,4 @@
-# Returns a list of guesses given a clue, and a set of possible words.
-def operative_guesses(model,words,clue):
-    similarities_with_clue = [(model.similarity(w,clue),w) for w in words]
-    return sorted(similarities_with_clue,reverse=True)
+# Returns a list of guesses given a clue, and a set of possible codenames.
+def operative_guesses(model, codenames, clue):
+    similarities_with_clue = [(model.similarity(w, clue), w) for w in codenames]
+    return sorted(similarities_with_clue, reverse=True)
